edc76d01640001b2aabf6603d114cfa0fd786a2f
vincent ndubi <vinnindubi@gmail.com>
2026-08-18 12:43:48 +0300

    Simplify vite config to standard export

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,25 +1,13 @@
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '');
-
   return {
     plugins: [
       react(), 
       tailwindcss()
-    ],
-    server: {
-      port: 5173,
-      proxy: {
-        '/api': {
-          target: env.VITE_API_URL,
-          changeOrigin: true,
-          secure: false,
-        }
-      }
-    }
+    ]
   }
 })
