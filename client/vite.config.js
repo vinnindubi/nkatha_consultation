94b788f71827c9e51f4bb1087b4e96e01ca068ca
vincent ndubi <vinnindubi@gmail.com>
2026-08-18 13:07:02 +0300

    Modified vite config file

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,13 +1,11 @@
-import { defineConfig} from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
-export default defineConfig(({ mode }) => {
-  return {
-    plugins: [
-      react(), 
-      tailwindcss()
-    ]
-  }
+export default defineConfig({
+  plugins: [
+    react(), 
+    tailwindcss()
+  ],
 })
