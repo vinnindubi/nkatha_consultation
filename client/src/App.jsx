9365e5e4e91a634ddd0c946cc6b462f76bc0d581
vincent ndubi <vinnindubi@gmail.com>
2026-08-27 12:34:50 +0300

    added vercel analytics and speed insights

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,8 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { useState, useEffect } from 'react';
-
+import {Analytics} from '@vercel/analytics/react';
+import { SpeedInsights } from "@vercel/speed-insights/react";
 // Layout Components
 import Navbar from './components/layout/Navbar';
 import Footer from './components/layout/Footer';
@@ -91,7 +92,8 @@ export default function App() {
           </main>
           
           <Footer />
-
+          <Analytics />
+          <SpeedInsights />
         </div>
       </BrowserRouter>
     </QueryClientProvider>
