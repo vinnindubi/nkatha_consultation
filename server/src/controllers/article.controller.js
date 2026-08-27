df7821fa964c612da2e72fea43d69f2c117797b1
vincent ndubi <vinnindubi@gmail.com>
2026-08-27 15:50:00 +0300

    updated deletearticle controller

--- server/src/controllers/article.controller.js
+++ server/src/controllers/article.controller.js
@@ -128,7 +128,7 @@ export const updateArticleStatus = async (req, res, next) => {
 };
 export const deleteArticle = async (req,res,next)=>{
   try{
-    const{id}= req.params()
+    const{id}= req.params;
     const deleted = await prisma.article.delete({
       where:{id}
     }    
