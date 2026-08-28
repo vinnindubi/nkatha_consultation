331a98bb10d88857d5d18887f801fd4895f8f874
vincent ndubi <vinnindubi@gmail.com>
2026-08-28 23:38:39 +0300

    updating update article router v2

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -89,7 +89,7 @@ export default function App() {
                 } 
               />
               <Route 
-                path="/admin/blog/edit/:id" 
+                path="/admin/blog/edit/:slug" 
                 element={
                   <ProtectedAdminRoute>
                     <BlogManager />
